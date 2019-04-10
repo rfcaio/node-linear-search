81dc3e4406c3c8980c53a957568eda1234ff532c
Caio Rodrigues <rfcaio90@gmail.com>
2019-04-10 13:25:28 -0300

    add better linear search algorithm

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
-const linearSearch = (array, size, element) => {
+const betterLinearSearch = (array, size, key) => {
+  for (let i = 0; i < size; i += 1) {
+    if (array[i] === key) {
+      return i
+    }
+  }
+  return null
+}
+
+const linearSearch = (array, size, key) => {
   let result = null
   for (let i = 0; i < size; i += 1) {
-    if (array[i] === element) {
+    if (array[i] === key) {
       result = i
     }
   }
   return result
 }
 
-module.exports = { linearSearch }
+module.exports = { betterLinearSearch, linearSearch }
--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 const test = require('ava')
 
-const { linearSearch } = require('./')
+const { betterLinearSearch, linearSearch } = require('./')
 
 let arr = []
 
 test.beforeEach(t => {
   arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+})
+
+test('betterLinearSearch should return the index of the first element found', t => {
+  t.is(betterLinearSearch(arr, arr.length, 0), 0)
+})
+
+test('betterLinearSearch should return null if element not found', t => {
+  t.is(betterLinearSearch(arr, arr.length, -1), null)
 })
 
 test('linearSearch should return the index of the last element found', t => {
